fcfb003144f163a0b1037c1db4b571a97e691078
Tomas Alves de Souza <tomas.alves08@gmail.com>
2024-03-05 22:33:59 +1300

    Add i18n library for page translations. Just did a couple of translations for testing purposes.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -30,6 +30,11 @@
             </li>
           </template>
         </ul>
+        <ul class="ml-auto">
+          <li>
+            <a class="px-2" href="#" @click.prevent="changeLocale"> {{ currentLocale }}</a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -42,7 +47,10 @@ import useUserStore from '@/stores/user'
 export default {
   name: 'AppHeader',
   computed: {
-    ...mapStores(useModalStore, useUserStore)
+    ...mapStores(useModalStore, useUserStore),
+    currentLocale() {
+      return this.$i18n.locale === 'pt' ? 'Portugues' : 'English'
+    }
   },
   methods: {
     toggleAuthModal() {
@@ -52,6 +60,9 @@ export default {
       this.userStore.signout()
 
       if (this.$route.meta.requiresAuth) this.$router.push({ name: 'home' })
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === 'pt' ? 'en' : 'pt'
     }
   }
 }
